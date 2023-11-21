09ab169a626c018008e89feedfad8021ae83328b
Pedro Costa <antonhyonhi@gmail.com>
2023-11-21 17:37:33 -0300

    Initial version.

---
+++ av_api.py
@@ -0,0 +1,112 @@
+
+import requests as http
+from time import sleep
+from io import StringIO
+import pandas as pd
+import json
+
+AV_ENDPOINT = 'https://alphavantage.co/query'
+
+class AlphaVantage:
+
+    def __init__(self, api_key=None) -> None:
+        self._api_key = api_key or get_api_key()
+
+    def symbol_search_query(self, keywords, data_type='json'):
+        params = params = f'function=SYMBOL_SEARCH&apikey={self._api_key}&keywords={keywords}'
+        if data_type == 'csv' or data_type == 'json':
+            params += f'&datatype={data_type}'
+        else:
+            raise Exception(f'Invalid data type: {data_type}')
+        return f'{AV_ENDPOINT}?{params}'
+
+    def intraday_query(self, symbol, interval_mins, output_size='compact', data_type='csv'):
+        params = f'function=TIME_SERIES_INTRADAY&apikey={self._api_key}&symbol={symbol}'
+        if interval_mins in [1, 5, 30, 60, '1', '5', '30', '60']:
+            params += f'&interval={interval_mins}min'
+        else:
+            raise Exception(f'Invalid interval: {interval_mins}')
+        if output_size == 'compact' or output_size == 'full':
+            params += f'&outputsize={output_size}'
+        else:
+            raise Exception(f'Invalid output size: {output_size}')
+        if data_type == 'csv' or data_type == 'json':
+            params += f'&datatype={data_type}'
+        else:
+            raise Exception(f'Invalid data type: {data_type}')
+        return f'{AV_ENDPOINT}?{params}'
+
+    def daily_adjusted_query(self, symbol, output_size='compact', data_type='csv'):
+        params = f'function=TIME_SERIES_DAILY_ADJUSTED&apikey={self._api_key}&symbol={symbol}'
+        if output_size == 'compact' or output_size == 'full':
+            params += f'&outputsize={output_size}'
+        else:
+            raise Exception(f'Invalid output size: {output_size}')
+        if data_type == 'csv' or data_type == 'json':
+            params += f'&datatype={data_type}'
+        else:
+            raise Exception(f'Invalid data type: {data_type}')
+        return f'{AV_ENDPOINT}?{params}'
+
+    def company_overview_query(self, symbol):
+        params = f'function=OVERVIEW&apikey={self._api_key}&symbol={symbol}'
+        return f'{AV_ENDPOINT}?{params}'
+
+    def income_statement_query(self, symbol):
+        params = f'function=INCOME_STATEMENT&apikey={self._api_key}&symbol={symbol}'
+        return f'{AV_ENDPOINT}?{params}'
+
+    def balance_sheet_query(self, symbol):
+        params = f'function=BALANCE_SHEET&apikey={self._api_key}&symbol={symbol}'
+        return f'{AV_ENDPOINT}?{params}'
+
+    def cash_flow_query(self, symbol):
+        params = f'function=CASH_FLOW&apikey={self._api_key}&symbol={symbol}'
+        return f'{AV_ENDPOINT}?{params}'
+
+    def earnings_query(self, symbol):
+        params = f'function=EARNINGS&apikey={self._api_key}&symbol={symbol}'
+        return f'{AV_ENDPOINT}?{params}'
+
+def get_api_key():
+    with open('api-key.txt') as file:
+        return file.read()
+
+def fetch_data(url: str) -> str:
+    sleep(12)
+    req = http.get(url)
+    if req.status_code != 200:
+        raise Exception(f'Request error: {req.status_code} - {req.content}')
+    data = str(req.content, 'utf-8')
+    check_api_error(data, url)
+    return data
+
+def check_api_error(data: str, url: str):
+    try: # Happy path is when this when Exception is thrown
+        err_msg = json.loads(data)['Error Message']
+        raise Exception(f'API error!\nMessage: {err_msg}\nURL: {url}')
+    except:
+        pass
+
+def decode_price_data(data: str) -> pd.DataFrame:
+    return pd.read_csv(StringIO(data))
+
+def decode_earnings_data(data: str) -> pd.DataFrame:
+    data = json.loads(data)
+    if 'quarterlyEarnings' in data:
+        data = data['quarterlyEarnings']
+    else:
+        data = data['annualEarnings']
+    return pd.DataFrame(data)
+
+def decode_fundamentals(data: str) -> pd.DataFrame:
+    data = json.loads(data)
+    if 'quarterlyEarnings' in data:
+        data = data['quarterlyReports']
+    else:
+        data = data['annualReports']
+    return pd.DataFrame(data)
+
+def decode_company_data(data: str) -> pd.DataFrame:
+    data = json.loads(data)
+    return pd.DataFrame([data])
---
+++ av_fetch.py
@@ -0,0 +1,161 @@
+from av_api import *
+from av_load import load_data
+from os.path import exists
+import pandas as pd
+import json
+from io import StringIO
+
+DATA_DIR = './data/alpha_vantage'
+
+def store_data(data: pd.DataFrame, table: str, tick: str):
+    path = f'{DATA_DIR}/{table}/{tick}.ftr'
+    return data.to_feather(path)
+
+def table_exists(table: str, tick: str) -> bool: 
+    path = f'{DATA_DIR}/{table}/{tick}.ftr'
+    return exists(path)
+
+def get_tick_list(list_path):
+    with open(list_path) as file:
+        ticks = []
+        for line in file.readlines():
+            ticks.append(line.rstrip())
+        return ticks
+
+class Warehouse:
+
+    def __init__(self, api_key=None) -> None:
+        self._api = AlphaVantage(api_key)
+        self._data_dir = DATA_DIR
+
+    def init_price_data(self, tick):
+        table = 'price_data'
+        if not table_exists(table, tick):
+            query = self._api.daily_adjusted_query(tick, data_type='csv', output_size='full')
+            data = fetch_data(query)
+            data = decode_price_data(data)
+            store_data(data, table, tick)
+
+    def init_overview_data(self, tick):
+        table = 'company_data'
+        if not table_exists(table, tick):
+            query = self._api.company_overview_query(tick)
+            data = fetch_data(query)
+            data = decode_company_data(data)
+            store_data(data, table, tick)
+
+    def init_income_statement(self, tick):
+        table = 'income_statements'
+        if not table_exists(table, tick):
+            query = self._api.income_statement_query(tick)
+            data = fetch_data(query)
+            data = decode_fundamentals(data)
+            store_data(data, table, tick)
+
+    def init_balance_sheet(self, tick):
+        table = 'balance_sheets'
+        if not table_exists(table, tick):
+            query = self._api.balance_sheet_query(tick)
+            data = fetch_data(query)
+            data = decode_fundamentals(data)
+            store_data(data, table, tick)
+
+    def init_cashflow_data(self, tick): 
+        table = 'cashflow_data'
+        if not table_exists(table, tick):
+            query = self._api.cash_flow_query(tick)
+            data = fetch_data(query)
+            data = decode_fundamentals(data)
+            store_data(data, table, tick)
+
+    def init_earnings_data(self, tick): 
+        table = 'earnings_data'
+        if not table_exists(table, tick):
+            query = self._api.earnings_query(tick)
+            data = fetch_data(query)
+            data = decode_earnings_data(data)
+            store_data(data, table, tick)
+
+    def update_price_data(self, tick):
+        table = 'price_data'
+        if table_exists(table, tick):
+            current_data = load_data(table, tick)
+            current_data.set_index('timestamp', inplace=True)
+
+            query = self._api.daily_adjusted_query(tick, data_type='csv', output_size='full')
+            new_data = fetch_data(query)
+            new_data = decode_price_data(new_data)
+            new_data.set_index('timestamp', inplace=True)
+
+            final_data = pd.concat([current_data, new_data], axis='index')
+            final_data.drop_duplicates(inplace=True)
+            final_data.sort_index(ascending=False, inplace=True)
+
+            store_data(final_data.reset_index(), table, tick)
+
+    def update_earnings_data(self, tick): 
+        table = 'earnings_data'
+        if table_exists(table, tick):
+            current_data = load_data(table, tick)
+            current_data.set_index('timestamp', inplace=True)
+
+            query = self._api.earnings_query(tick)
+            data = fetch_data(query)
+            store_data(data, table, tick)
+
+            new_data = fetch_data(query)
+            new_data = decode_earnings_data(new_data)
+            new_data.set_index('timestamp', inplace=True)
+
+    def update_overview_data(self, tick):
+        table = 'company_data'
+        #overview_path = f'{DATA_FOLDER}/company_data/{tick}.json'
+        if table_exists(table, tick):
+            query = self._api.company_overview_query(tick)
+            data = fetch_data(query)
+            store_bytes(data, overview_path)
+
+    def update_income_statement(self, tick):
+        table = 'income_statements'
+        #income_path = f'{DATA_FOLDER}/income_statements/{tick}.json'
+        if table_exists(table, tick):
+            query = self._api.income_statement_query(tick)
+            data = fetch_data(query)
+            store_bytes(data, income_path)
+
+    def update_balance_sheet(self, tick):
+        table = 'balance_sheets'
+        #balance_path = f'{DATA_FOLDER}/balance_sheets/{tick}.json'
+        if table_exists(table, tick):
+            query = self._api.balance_sheet_query(tick)
+            data = fetch_data(query)
+            store_bytes(data, balance_path)
+
+    def update_cashflow_data(self, tick):
+        table = 'cashflow_data'
+        #cashflow_path = f'{DATA_FOLDER}/cashflow_data/{tick}.json'
+        if table_exists(table, tick):
+            query = self._api.cash_flow_query(tick)
+            data = fetch_data(query)
+            store_bytes(data, cashflow_path)
+
+
+if __name__ == '__main__':
+    warehouse = Warehouse()
+    ticks = get_tick_list(f'{DATA_DIR}/tick_list.txt')
+
+    for tick in ticks:
+        warehouse.init_price_data(tick)
+        warehouse.init_overview_data(tick)
+        warehouse.init_income_statement(tick)
+        warehouse.init_balance_sheet(tick)
+        warehouse.init_cashflow_data(tick)
+        warehouse.init_earnings_data(tick)
+
+        #warehouse.update_overview_data(tick)
+        #warehouse.update_income_statement(tick)
+        #warehouse.update_balance_sheet(tick)
+        #warehouse.update_cashflow_data(tick)
+        warehouse.update_earnings_data(tick)
+        warehouse.update_price_data(tick)
+    
