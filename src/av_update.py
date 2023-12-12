099c597dcea3a8cd10465be9ae5086d112359211
Pedro Costa <antonhyonhi@gmail.com>
2023-12-12 16:46:04 -0300

    Split the main loop to update one table at a time, instead of one company at a time.

--- src/av_update.py
+++ src/av_update.py
@@ -1,26 +1,30 @@
 import src.av_api as av
 import src.av_warehouse as wh
 
+WAREHOUSE_DIRECTORY = 'data/'
+
 def run():
     api = av.AlphaVantage()
-    warehouse = wh.Warehouse(wh.connect('data/warehouse.db'))
+    warehouse = wh.Warehouse(wh.connect(f'{WAREHOUSE_DIRECTORY}warehouse.db'))
     ticks = warehouse.list_keys('companies_data')
 
     for (tick,) in ticks:
-        #warehouse.init_price_data(tick)
-        #warehouse.init_overview_data(tick)
-        #warehouse.init_income_statement(tick)
-        #warehouse.init_balance_sheet(tick)
-        #warehouse.init_cashflow_data(tick)
-        #warehouse.init_earnings_data(tick)
+        update_company_data(api, warehouse, tick)
 
-        #warehouse.update_overview_data(tick)
-        #warehouse.update_income_statement(tick)
-        #warehouse.update_balance_sheet(tick)
-        #warehouse.update_cashflow_data(tick)
-        #update_company_data(api, warehouse, tick)
-        update_price_data(api, warehouse, tick)
+    for (tick,) in ticks:
         update_earnings_data(api, warehouse, tick)
+
+    for (tick,) in ticks:
+        update_cashflow_data(api, warehouse, tick)
+
+    for (tick,) in ticks:
+        update_balance_sheet(api, warehouse, tick)
+    
+    for (tick,) in ticks:
+        update_income_statement(api, warehouse, tick)
+
+    for (tick,) in ticks:
+        update_price_data(api, warehouse, tick)
 
 def update_price_data(api: av.AlphaVantage, warehouse: wh.Warehouse, tick: str) -> None:
     table = 'price_data'
@@ -61,7 +65,6 @@ def update_company_data(api: av.AlphaVantage, warehouse: wh.Warehouse, tick: str
     table = 'company_data'
     data = api.get_company_overview(tick)
     data = av.decode_company_data(data) 
-    #data = [(tick,) + row for row in data]
     warehouse.update_table(table, data)
 
 if __name__ == '__main__':
