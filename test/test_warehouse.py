b1f2d82aa42306d6efa2c3f8fb07fcf24f47132b
Pedro Costa <antonhyonhi@gmail.com>
2023-12-19 13:08:56 -0300

    Refactored warehouse test file

--- test/test_warehouse.py
+++ test/test_warehouse.py
@@ -1,6 +1,23 @@
 from unittest import TestCase, main
+import sqlite3 as sql3
+from src.av_warehouse import Warehouse
+class WarehouseSetup:
+    def __init__(self) -> None:
 
-from setup import WarehouseSetup
+        self.table = 'test_table'
+        self.empty_table = 'empty_table'
+
+        self.old_data = [('AAPL', 1), ('NVDA', 2), ('IBM', 3)]
+        self.new_data = [('TSLA', 4), ('GOOG', 5), ('AMD', 6)]
+
+        test_db = sql3.connect(':memory:')
+        test_db.execute(f'CREATE TABLE {self.table} (key PRIMARY KEY, value)')
+        test_db.execute(f'CREATE TABLE {self.empty_table} (key PRIMARY KEY, value)')
+
+        test_db.executemany(f'INSERT INTO {self.table} VALUES (?, ?)', self.old_data)
+        test_db.commit()
+
+        self.warehouse = self.warehouse = Warehouse(test_db)
 
 class ExtendTable(TestCase):
 
