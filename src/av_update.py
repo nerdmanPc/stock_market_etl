bf9a006e4b9418c586622b0bfa1e02032789a360
Pedro Costa <antonhyonhi@gmail.com>
2023-12-28 13:14:53 -0300

    Refactored av_update and changed the order of updates

--- src/av_update.py
+++ src/av_update.py
@@ -16,71 +16,71 @@ def run():
     if not '--skip_overview' in shell_args: 
         update_company_data(api, warehouse, ticks)
     update_earnings_data(api, warehouse, ticks)
+    update_income_statement(api, warehouse, ticks)
     update_cashflow_data(api, warehouse, ticks)
     update_balance_sheet(api, warehouse, ticks)
-    update_income_statement(api, warehouse, ticks)
     update_price_data(api, warehouse, ticks)
 
-def update_price_data(api: av.AlphaVantage, warehouse: wh.Warehouse, ticks: list[str], current_date: date = None) -> None:
+def update_price_data(api: av.AlphaVantage, warehouse: wh.Warehouse, ticks: list[str], limit_date: date = None) -> None:
     table = 'price_data'
-    current_date = current_date or date.today()
+    limit_date = limit_date or date.today()
     update_period = timedelta(days=7)
     for tick in ticks:
         last_update = warehouse.latest_timestamp(table, tick)
         next_update = last_update + update_period
-        if current_date < next_update:
+        if limit_date < next_update:
             continue
         data = api.get_weekly_adjusted(tick)
         data = av.decode_price_data(data, tick) 
         warehouse.extend_table(table, data)
 
-def update_earnings_data(api: av.AlphaVantage, warehouse: wh.Warehouse, ticks: list[str], current_date: date = None) -> None:
+def update_earnings_data(api: av.AlphaVantage, warehouse: wh.Warehouse, ticks: list[str], limit_date: date = None) -> None:
     table = 'earnings_data'
-    current_date = current_date or date.today()
+    limit_date = limit_date or date.today()
     update_period = timedelta(days=90)
     for tick in ticks:
         last_update = warehouse.latest_timestamp(table, tick)
         next_update = last_update + update_period
-        if current_date < next_update:
+        if limit_date < next_update:
             continue
         data = api.get_earnings(tick)
         data = av.decode_earnings_data(data, tick)
         warehouse.extend_table(table, data)
 
-def update_cashflow_data(api: av.AlphaVantage, warehouse: wh.Warehouse, ticks: list[str], current_date: date = None) -> None:
+def update_cashflow_data(api: av.AlphaVantage, warehouse: wh.Warehouse, ticks: list[str], limit_date: date = None) -> None:
     table = 'cashflow_data'
-    current_date = current_date or date.today()
+    limit_date = limit_date or date.today()
     update_period = timedelta(days=90)
     for tick in ticks:
         last_update = warehouse.latest_timestamp(table, tick)
         next_update = last_update + update_period
-        if current_date < next_update:
+        if limit_date < next_update:
             continue
         data = api.get_cash_flow(tick)
         data = av.decode_fundamentals(data, tick)
         warehouse.extend_table(table, data)
 
-def update_income_statement(api: av.AlphaVantage, warehouse: wh.Warehouse, ticks: list[str], current_date: date = None) -> None:
+def update_income_statement(api: av.AlphaVantage, warehouse: wh.Warehouse, ticks: list[str], limit_date: date = None) -> None:
     table = 'income_statement'
-    current_date = current_date or date.today()
+    limit_date = limit_date or date.today()
     update_period = timedelta(days=90)
     for tick in ticks:
         last_update = warehouse.latest_timestamp(table, tick)
         next_update = last_update + update_period
-        if current_date < next_update:
+        if limit_date < next_update:
             continue
         data = api.get_income_statement(tick)
         data = av.decode_fundamentals(data, tick)
         warehouse.extend_table(table, data)
 
-def update_balance_sheet(api: av.AlphaVantage, warehouse: wh.Warehouse, ticks: list[str], current_date: date = None) -> None:
+def update_balance_sheet(api: av.AlphaVantage, warehouse: wh.Warehouse, ticks: list[str], limit_date: date = None) -> None:
     table = 'balance_sheet'
-    current_date = current_date or date.today()
+    limit_date = limit_date or date.today()
     update_period = timedelta(days=90)
     for tick in ticks:
         last_update = warehouse.latest_timestamp(table, tick)
         next_update = last_update + update_period
-        if current_date < next_update:
+        if limit_date < next_update:
             continue
         data = api.get_balance_sheet(tick)
         data = av.decode_fundamentals(data, tick)
